75de5665048258b053da2c69a85f49574dbd2bfa
Fillip Hudson <Filliphudson222@gmail.com>
2022-07-15 21:25:04 -0300

    Primeira parte feita

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -3,19 +3,14 @@ import './styles.css'
 import {FaWind, FaCloudSun, FaTemperatureLow} from 'react-icons/fa'
 import {WiHumidity} from 'react-icons/wi'
 
-
-
-function App() {
+function WeatherApp() {
   const [data, setData] = useState({})
   const [location, setlocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=c94d80840abbfc5e33bb59cbe38c477b&lang=pt_br`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=pt_br&appid=c94d80840abbfc5e33bb59cbe38c477b`
 
-  const searchLocation = (event) => {
+  const pesquisaLocalizacao = (event) => {
     if(event.key === 'Enter'){
-      if(location != ){
-        console.log('Lugar inválido')
-      }else{
         fetch(url)
         .then(resp => resp.json())
         .then((data) => {
@@ -24,79 +19,79 @@ function App() {
         .catch((err) => 'erro não encontrado' + err)
       setlocation('')
       }
-      }
-        
     }
     
   return (
-    <div className="app">
+    <div className="weather-app">
       <h1>Weather app</h1>
 
       <div className='pesquisa'>
         <input
         value={location}
         onChange={event => setlocation(event.target.value)}
-        onKeyPress={searchLocation}
+        onKeyPress={pesquisaLocalizacao}
         placeholder="Cidade..."
         type='text'
         ></input>
-        {}
       </div>
 
       {data.name != undefined && 
-       <>
-      <div className="container">
+    <>
+      <div className="container-dados">
+
         <div className="topo">
           <div className="localizacao">
             <h2>{data.name}</h2>
           </div>
-          <div className="temp">
+          <div className="temperatura">
             {data.main ?  <h2>{data.main.temp.toFixed()}°c</h2> : null}
           </div>
         </div>
         
-          <div className="informacoes">
+        <div className="informacoes">
           <h3>Informações do tempo</h3>
 
-            <div className="descricao">
-              <FaCloudSun/>
-              <div>
-                {data.weather ? <p>{data.weather[0].main}</p> : null}
-                <span>Tempo</span>
-              </div>
-            </div>
-            <div className="sensacao">
-              <FaTemperatureLow/>
-              <div>
-                {data.main ? <p>{data.main.feels_like.toFixed()}°c</p> : null}
-                <span>Sensação</span>
-                </div>
-            </div>
-            <div className="humidity">  
-              <WiHumidity/>
-              <div>
-              {data.main ? <p>{data.main.humidity.toFixed()}%</p> : null}
-              <span>Humidade</span>
-                </div>
-            </div>
-            <div className="wind">
-              <FaWind/>
-              <div>
-              {data.wind ? <p>{data.wind.speed.toFixed()} MPH</p> : null}
-              <span>Vento</span>
-                </div>
+          <div className="descricao">
+            <FaCloudSun/>
+            <div>
+              {data.weather ? <p>{data.weather[0].description}</p> : null}
+              <span>Tempo</span>
             </div>
           </div>
+
+          <div className="sensacao">
+            <FaTemperatureLow/>
+            <div>
+              {data.main ? <p>{data.main.feels_like.toFixed()}°c</p> : null}
+              <span>Sensação</span>
+            </div>
+          </div>
+
+          <div className="humidade">  
+            <WiHumidity/>
+            <div>
+              {data.main ? <p>{data.main.humidity.toFixed()}%</p> : null}
+              <span>Humidade</span>
+            </div>
+          </div>
+          
+          <div className="vento">
+            <FaWind/>
+            <div>
+              {data.wind ? <p>{data.wind.speed.toFixed()} MPH</p> : null}
+              <span>Vento</span>
+            </div>
+          </div>
+        </div>
       </div>
+      
       <div className='copy'>
         <span>&copy; Feito por @Fillip_Hudson</span>
       </div>
-      </>
+    </>
       }
-      
-
-    </div>
+  </div>
   );
 }
 
-export default App;
+export default WeatherApp;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import WeatherApp from './WeatherApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <WeatherApp />
   </React.StrictMode>
 );
 
